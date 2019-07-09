7412d8dc3be3362beb09bff4dbb86f4b372546ac
iwhonduras <iwhnpruebas@gmail.com>
2019-07-09 14:17:14 -0600

    parte administrativa terminada, sin tanto detalle.

--- futeca/src/Admin/Dashboard.js
+++ futeca/src/Admin/Dashboard.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react';
 import { Container, Navbar, Nav, Row, Col, Table } from 'react-bootstrap';
 
-import logo from '../img/futeca.png'
-
+import logo from '../img/futeca.png';
+// import logoAcerca from '../img/facerca.png';
+import logoSede from '../img/falquiler.png';
+import logoAcademia from '../img/facademia.png';
+import logoCampeonato from '../img/fcampeonato.png';
+// import logoContacto from '../img/fcontactanos.png';
 import './Dashboard.css';
 
 class Dashboard extends Component {
+    constructor(){
+        super();
+        this.state = {
+            seccion: "INICIO"
+        }
+    }
     render() {
+        const NavBar = (
+            <Navbar className="navbar" id="menu" bg="light" expand="lg">
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link to="/futeca" onClick={() => {this.setState({seccion:"INICIO"});}}><img className="logo-secciones" src={logo} alt="INICIO"/></Nav.Link>
+                    <Nav.Link href="#link" onClick={() => {this.setState({seccion:"ACERCA DE"});}}><img className="logo-secciones"  src={logo} alt="ACERCA DE"/></Nav.Link>
+                    <Nav.Link href="#link" onClick={() => {this.setState({seccion:"ALQUILER"});}}><img className="logo-secciones" src={logoSede} alt="ALQUILER"/></Nav.Link>
+                    <Nav.Link href="#link" onClick={() => {this.setState({seccion:"ACADEMIA"});}}><img className="logo-secciones" src={logoAcademia} alt="ACADEMIA"/></Nav.Link>
+                    <Nav.Link href="#link" onClick={() => {this.setState({seccion:"CAMPEONATO"});}}><img className="logo-secciones" src={logoCampeonato} alt="CAMPEONATOS"/></Nav.Link>
+                    <Nav.Link href="#link" onClick={() => {this.setState({seccion:"CONTÁCTO"});}}><img className="logo-secciones-final"  src={logo} alt="CONTÁCTO"/></Nav.Link>
+                    <Nav.Link href="#link">Salir</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          );
         return (
             <Container>
-                <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="/dashboard"><img src={logo} alt="Futeca" /></Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Salir</Nav.Link>
-                    </Nav>
-                </Navbar>
+                {NavBar}
                 <Row>
-                    <Col>
-                        <Nav defaultActiveKey="/home" className="flex-column">
-                            <Nav.Link href="/home">Active</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-2">Link</Nav.Link>
-                            <Nav.Link eventKey="disabled" disabled>Disabled</Nav.Link>
-                        </Nav>
-                    </Col>
+                    <h1 className="tituloDinamico">ADMINISTRACION DE {this.state.seccion}</h1>
+                </Row>
+                <Row>
                     <Col>
                         <Container>
                             <Table striped bordered hover>
@@ -50,7 +66,8 @@ class Dashboard extends Component {
                                     </tr>
                                     <tr>
                                         <td>3</td>
-                                        <td colSpan="2">Larry the Bird</td>
+                                        <td>Larry the Bird</td>
+                                        <td>--------------</td>
                                         <td>@twitter</td>
                                     </tr>
                                 </tbody>
